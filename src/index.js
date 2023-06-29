7cf72478b07f4314e1f2cd1273cb1b1ea91d711d
Farhan Franaka <farhanfranaka0505@gmail.com>
2023-06-29 17:15:51 +0700

    fix env

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 
 import axios from "axios";
 
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
